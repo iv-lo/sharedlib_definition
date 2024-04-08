b940052fbee69e33e83902b0e8a231f5e8753089
Ilia Lopatin <iv.lopatin@outlook.com>
2024-04-08 17:38:52 +0400

    update class

--- src/com/arrival/ucs/BundleHolderClazz.groovy
+++ src/com/arrival/ucs/BundleHolderClazz.groovy
@@ -73,7 +73,7 @@ class BundleHolderClazz extends ParentClazz {
         projectsMap.each { projectName, bundlesList ->
             bundlesList.each { Map bundleInfo ->
                 bundleInfo.each { projectPath, version ->
-                    this.addBundle(projectName, ProjectClazz(this.pipeline, projectPath, version, "now")) //projectPath, version
+                    this.addBundle(projectName, new ProjectClazz(this.pipeline, projectPath, version, "now")) //projectPath, version
                 }
             }
         }
