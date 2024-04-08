db9574ba040d4e64e289ebfe77c954d83df873aa
Ilia Lopatin <iv.lopatin@outlook.com>
2024-04-08 17:35:25 +0400

    update class

--- src/com/arrival/ucs/ProjectClazz.groovy
+++ src/com/arrival/ucs/ProjectClazz.groovy
@@ -7,17 +7,20 @@ class ProjectClazz extends ParentClazz {
     String version
     String commit
     // , String project, String version
-    ProjectClazz(def pipeline) {
+    ProjectClazz(def pipeline, String project, String version, String commit) {
         super(pipeline)
+        this.project = project
+        this.version = version
+        this.commit = commit
     }
 
-    ProjectClazz createProject(String project, String version, String commit) {
-        ProjectClazz projectObj = ProjectClazz(this)
-        projectObj.project = project
-        projectObj.version = version
-        projectObj.commit = commit
-        return projectObj
-    }
+    // ProjectClazz createProject(String project, String version, String commit) {
+    //     ProjectClazz projectObj = ProjectClazz(this)
+    //     projectObj.project = project
+    //     projectObj.version = version
+    //     projectObj.commit = commit
+    //     return projectObj
+    // }
 
     String toJsonString() {
         return "{\"${project}\": \"${version}\"}"
