c7c87a82de668cf33f9aaa72843a79f89b9628a7
Ilia Lopatin <iv.lopatin@outlook.com>
2024-04-08 15:55:14 +0400

    update class

--- src/com/arrival/ucs/BundleHolderClazz.groovy
+++ src/com/arrival/ucs/BundleHolderClazz.groovy
@@ -5,7 +5,7 @@ import groovy.json.JsonSlurper
 
 
 class BundleHolderClazz extends ParentClazz {
-    // protected Map<String, List<ProjectClazz>> _bundles = [:]
+    protected Map<String, List<ProjectClazz>> _bundles = [:]
     // String initBundles = """
     //         {
     //     "Van_1": [
@@ -43,46 +43,46 @@ class BundleHolderClazz extends ParentClazz {
         super(pipeline)
     }
 
-    // void addBundle(String projectName, ProjectClazz bundle) {
-    //     if (!_bundles.containsKey(projectName)) {
-    //         _bundles[projectName] = []
-    //     }
-    //     _bundles[projectName].add(bundle)
-    // }
+    void addBundle(String projectName, ProjectClazz bundle) {
+        if (!_bundles.containsKey(projectName)) {
+            _bundles[projectName] = []
+        }
+        _bundles[projectName].add(bundle)
+    }
 
-    // String toJsonString() {
-    //     def bundleStrings = _bundles.collect { projectName, bundleList ->
-    //         def bundleStrings = bundleList.collect { bundle ->
-    //             "${bundle.toJsonString()}"
-    //         }.join(',\n    ')
+    String toJsonString() {
+        def bundleStrings = _bundles.collect { projectName, bundleList ->
+            def bundleStrings = bundleList.collect { bundle ->
+                "${bundle.toJsonString()}"
+            }.join(',\n    ')
             
-    //         "\"${projectName}\": [\n    ${bundleStrings}\n]"
-    //     }.join(",\n") 
+            "\"${projectName}\": [\n    ${bundleStrings}\n]"
+        }.join(",\n") 
 
-    //     return "{\n${bundleStrings}\n}"
-    // }
+        return "{\n${bundleStrings}\n}"
+    }
 
-    // void initializeFromString(String bundlesProjectsText) {
-    //     _bundles.clear()
-    //     def jsonSlurper = new JsonSlurper()
-    //     def projectsMap = jsonSlurper.parseText(bundlesProjectsText)
+    void initializeFromString(String bundlesProjectsText) {
+        _bundles.clear()
+        def jsonSlurper = new JsonSlurper()
+        def projectsMap = jsonSlurper.parseText(bundlesProjectsText)
 
-    //     projectsMap.each { projectName, bundlesList ->
-    //         bundlesList.each { Map bundleInfo ->
-    //             bundleInfo.each { projectPath, version ->
-    //                 this.addBundle(projectName, new ProjectClazz(projectPath, version))
-    //             }
-    //         }
-    //     }
-    // }
+        projectsMap.each { projectName, bundlesList ->
+            bundlesList.each { Map bundleInfo ->
+                bundleInfo.each { projectPath, version ->
+                    this.addBundle(projectName, new ProjectClazz(projectPath, version))
+                }
+            }
+        }
+    }
 
-    // List<Map<String, String>> getBundleProjects(String key) {
-    //     return _bundles.getOrDefault(key, [])
-    //     // def projects = bundles.getOrDefault(key, [])
-    //     // String projectText = projects.collect { bundle ->
-    //     //     "${bundle.toWorkspaceCfgLink()}"
-    //     // }.join("\n")
-    //     // return projectText
-    // }
+    List<Map<String, String>> getBundleProjects(String key) {
+        return _bundles.getOrDefault(key, [])
+        // def projects = bundles.getOrDefault(key, [])
+        // String projectText = projects.collect { bundle ->
+        //     "${bundle.toWorkspaceCfgLink()}"
+        // }.join("\n")
+        // return projectText
+    }
 
 }
